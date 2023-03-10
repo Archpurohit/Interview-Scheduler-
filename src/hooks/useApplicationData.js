7b78fd477dc86325ccd67dd068192943844a8224
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-03-09 19:15:15 -0800

    updating spots remaining

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,67 +1,90 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-
-export default function useApplicationData(initial) {
-
-useEffect(() => {
-  Promise.all([
-    axios.get("/api/days"),
-    axios.get("/api/appointments"),
-    axios.get("/api/interviewers"),
-  ]).then((all) => {
-    console.log(all);
-    setState((prev) => ({
-      ...prev,
-      days: all[0].data,
-      appointments: all[1].data,
-      interviewers: all[2].data,
-    }));
+const useApplicationData = () => {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
   });
-}, []);
 
-function bookInterview(id, interview) {
-  // send PUT request to update appointment with interview data
-  const appointment = {
-    ...state.appointments[id],
-    interview: { ...interview },
-  };
-  const appointments = {
-    ...state.appointments,
-    [id]: appointment,
-  };
-  return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-    setState({
-      ...state,
-      appointments,
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
-  });
-}
+  }, []);
 
-function cancelInterview(id) {
-  const appointment = {
-    ...state.appointments[id],
-    interview: null,
+  const updateSpots = (day, days, appointments) => {
+    const dayObj = days.find((d) => d.name === day);
+
+    const spots = dayObj.appointments.filter((id) => !appointments[id].interview).length;
+
+    const newDay = { ...dayObj, spots };
+    const newDays = days.map((d) => (d.name === day ? newDay : d));
+
+    return newDays;
   };
-  const appointments = {
-    ...state.appointments,
-    [id]: appointment,
+
+  const bookInterview = (id, interview) => {
+    // send PUT request to update appointment with interview data
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    const days = updateSpots(state.day, state.days, appointments);
+
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      setState((prev) => ({
+        ...prev,
+        appointments,
+        days,
+      }));
+    });
   };
-  return axios.delete(`/api/appointments/${id}`).then(() => {
-    setState(...state, appointments);
-  });
-}
 
+  const cancelInterview = (id) => {
+    // send DELETE request to delete interview data from appointment
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    const days = updateSpots(state.day, state.days, appointments);
 
-const setDay = (day) => setState({ ...state, day });
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState((prev) => ({
+        ...prev,
+        appointments,
+        days,
+      }));
+    });
+  };
 
-const [state, setState] = useState({
-  day: "Monday",
-  days: [],
-  // you may put the line below, but will have to remove/comment hardcoded appointments variable
-  appointments: {},
-});
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+  };
+};
 
-return {setDay, cancelInterview, bookInterview, state};
-
-}
+export default useApplicationData;
